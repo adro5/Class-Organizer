79ecbb8bbb4e6d09b04d34eea1d7bf8713d601ee
Adam Robinson <adro5@users.noreply.github.com>
2018-08-06 13:13:03 -0400

    Updated Course class
    Removed member serialization

--- Course.cs
+++ Course.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace College_Organizer
 {
-    [JsonObject(MemberSerialization.Fields)]
     public class Course
     {
         public string courseName { get; set; }
